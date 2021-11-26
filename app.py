c5788729e72bcb66771116df20df9ff922040ff6
Natali Baranova <idigitalbrick@gmail.com>
2021-11-26 19:15:39 +0600

    Fix

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def task_by_email():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
+    app.run('0.0.0.0', port=int(os.environ.get('PORT', 5000)))
